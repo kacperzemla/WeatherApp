f57c72e886e9ae3d44b14664443efd784ce930f3
Kacper <kacperpeper@gmail.com>
2022-04-04 21:57:45 +0200

    created containers for web services

--- src/ServiceContainer.js
+++ src/ServiceContainer.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import useFetch from "./useFetch";
+
 import WeatherData from "./WeatherData";
 
 function ServiceContainer() {
-
+    const [temp, setTemp ] = useState(0);
+    const [wind, setWind] = useState(10);
+    const [humidity, setHumidity] = useState(5);
+// serwis/dzien -> 8/12/16/20/24
     return (
         <div className="service-container">
             <div className="media-container">
@@ -9,6 +15,20 @@ function ServiceContainer() {
                 <div className="service-weather__icon">icon</div>
             </div>
         <WeatherData />
+        <div className="days-container">
+            <div className="next-day">
+                <div className="next-day-icon"></div>
+                <p className="next-day-name">Mon.</p>
+            </div>
+            <div className="next-day">
+                <div className="next-day-icon"></div>
+                <p className="next-day-name">Tue.</p>
+            </div>
+            <div className="next-day">
+                <div className="next-day-icon"></div>
+                <p className="next-day-name">xdd</p>
+            </div>
+        </div>
         </div>
     );
 }
