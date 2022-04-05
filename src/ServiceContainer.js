18d9f2d7dc6d48b239ecf63642e9fbffed309f63
Kacper <kacperpeper@gmail.com>
2022-04-06 01:02:44 +0200

    added the functionality to check the weather for 3 days with useState

--- src/ServiceContainer.js
+++ src/ServiceContainer.js
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "./useFetch";
 
 import WeatherData from "./WeatherData";
 
 function ServiceContainer({ service }) {
-  console.log(service);
+
   const { data, isPending, error } = useFetch(
     "http://localhost:8000/" + service
   );
+
+  console.log(data)
+
+  const [temp, setTemp ] = useState(0);
+  const [wind, setWind ] = useState(0);
+  const [humidity, setHumidity ] = useState(0);
+  
+  useEffect(() => {
+    if(data){
+      setTemp(data[1].eightHour.temp);
+      setWind(data[1].eightHour.wind);
+      setHumidity(data[1].eightHour.humidity);
+    }
+  },[data])
+
+
+
+  function handleClick(i){
+    setTemp(data[i].eightHour.temp);
+    setWind(data[i].eightHour.wind);
+    setHumidity(data[i].eightHour.humidity);
+  };
+
 
   // serwis/dzien -> 8/12/16/20/24
   return (
@@ -19,17 +42,17 @@ function ServiceContainer({ service }) {
             <div className="service-logo">{data[0].serviceName}</div>
             <div className="service-weather__icon">icon</div>
           </div>
-          <WeatherData />
+          <WeatherData temp = { temp } wind = { wind } humidity = { humidity }/>
           <div className="days-container">
-            <div className="next-day">
+            <div className="next-day" onClick={() => handleClick(1)}>
               <div className="next-day-icon"></div>
               <p className="next-day-name">Mon.</p>
             </div>
-            <div className="next-day">
+            <div className="next-day" onClick={() => handleClick(2)}>
               <div className="next-day-icon"></div>
               <p className="next-day-name">Tue.</p>
             </div>
-            <div className="next-day">
+            <div className="next-day" onClick={() => handleClick(3)}>
               <div className="next-day-icon"></div>
               <p className="next-day-name">xdd</p>
             </div>
--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -1,17 +1,17 @@
-function WeatherData (){
+function WeatherData ({temp, wind, humidity}){
     return (
         <div className="weather-data">
         <div className="weather-data-item">
             <p className="weather-data-name">Temp.</p>
-            <p className="weather-data-value">35 &#8451;</p>
+            <p className="weather-data-value">{ temp } &#8451;</p>
         </div>
         <div className="weather-data-item">
             <p className="weather-data-name">Wind</p>
-            <p className="weather-data-value">10km/h</p>
+            <p className="weather-data-value">{ wind }km/h</p>
         </div>
         <div className="weather-data-item">
             <p className="weather-data-name">Humidity</p>
-            <p className="weather-data-value">75%</p>
+            <p className="weather-data-value">{ humidity }%</p>
         </div>
     </div>
     );
