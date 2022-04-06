b5763cd66074c23f0bd2839307feadc471b4a0d0
Kacper <kacperpeper@gmail.com>
2022-04-07 00:01:53 +0200

    craeted settings tab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "./Navbar";
-import Compare from './Compare';
-import Weather from './Weather';
+import Compare from "./Compare";
+import Weather from "./Weather";
+import Settings from "./Settings";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-        <Navbar />
-          <Routes>
-            <Route path ='/' element = {<Weather/>} />
-            <Route path ='/compare' element={<Compare/>} />
-          </Routes>
-        </Router>
+      <Router>
+        <div className="icon-container">
+          <Link to="/">
+            <FontAwesomeIcon icon={faHouse} className="settings-icon" />
+          </Link>
+          <Link to="/settings">
+            <FontAwesomeIcon icon={faGear} className="settings-icon" />{" "}
+          </Link>
+        </div>
+
+        {/* <Navbar /> */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Weather />
+              </>
+            }
+          />
+          <Route
+            path="/compare"
+            element={
+              <>
+                <Navbar />
+                <Compare />
+              </>
+            }
+          />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
